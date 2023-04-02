7c8a2443c032bb4a739876abfe0782e53017a1da
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-04-02 14:15:16 +0300

    Задание 2
    Выбор приложения

---
+++ share/src/main/java/com/mirea/schelukhinka/share/MainActivity.java
@@ -0,0 +1,27 @@
+package com.mirea.schelukhinka.share;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_TEXT, "Mirea");
+        startActivity(Intent.createChooser(intent, "Выбор за вами!"));
+    }
+}
